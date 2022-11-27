2a775af5d87fff16cd1447e25684f824fd36b7dd
Your Name <you@example.com>
2022-11-27 18:23:28 +0100

    add nodemailer setup

--- config/plugins.js
+++ config/plugins.js
@@ -1,21 +1,40 @@
 module.exports = ({ env }) => ({
-    'users-permissions': {
-      config: {
-      jwtSecret: env('JWT_SECRET'),
+  "users-permissions": {
+    config: {
+      jwtSecret: env("JWT_SECRET"),
+    },
+  },
+  upload: {
+    config: {
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
+      },
+      actionOptions: {
+        upload: {},
+        delete: {},
       },
     },
-    upload: {
-      config: {
-        provider: 'cloudinary',
-        providerOptions: {
-          cloud_name: env('CLOUDINARY_NAME'),
-          api_key: env('CLOUDINARY_KEY'),
-          api_secret: env('CLOUDINARY_SECRET'),
+  },
+
+  email: {
+    config: {
+      provider: "nodemailer",
+      providerOptions: {
+        host: env("SMTP_HOST", "smtp.example.com"),
+        port: env("SMTP_PORT", 587),
+        auth: {
+          user: env("SMTP_USERNAME"),
+          pass: env("SMTP_PASSWORD"),
         },
-        actionOptions: {
-          upload: {},
-          delete: {},
-        },
+        // ... any custom nodemailer options
+      },
+      settings: {
+        defaultFrom: "houssinmhamdi123@gmail.com",
+        defaultReplyTo: "houssinmhamdi123@gmail.com",
       },
     },
-  });
+  },
+});
--- config/server.js
+++ config/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
+  url: env("URL", "https://strapi-production-b99d.up.railway.app"),
   app: {
     keys: env.array("APP_KEYS"),
   },
